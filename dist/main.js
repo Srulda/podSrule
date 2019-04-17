e2395a23bd7be078e78434646674b9b0ce35568d
Danny <dannybrudner@gmail.com>
2019-04-17 17:21:51 -0400

    Fixed merge bugs

--- dist/main.js
+++ dist/main.js
@@ -5,11 +5,7 @@ const discoveryManager = new DiscoveryManager()
 
 $(document).ready(function () {
     $('.tabs').tabs()
-
-
-
-    $('.sidenav').sidenav({edge: 'right'})
-
+    $('.sidenav').sidenav({ edge: 'right' })
 })
 
 
@@ -29,13 +25,9 @@ $("body").on("click", ".player-play ", async function () {
 
     podManager.savedPlayedPod(id)
     renderer.renderListened(podManager.listenedPodcast)
-    let pod = podManager.getCorrectPod(id)    
+    let pod = podManager.getCorrectPod(id)
     pod.audioManager.audio.play()
 
-    console.log(id)
-    let pod = podManager.getCorrectPod(id)    
-    pod.audioManager.audio.play()
-  
     $(this).closest(".card").addClass("bounce-top")
 })
 
@@ -60,31 +52,31 @@ $("body").on("click", ".player-stop", function () {
 $("body").on("click", ".fa-forward", function () {
     let id = $(this).closest(".podcast").find(".episodeName").attr("id")
     let pod = podManager.getCorrectPod(id)
-    pod.audioManager.audio.currentTime +=30
+    pod.audioManager.audio.currentTime += 30
 })
 
 $("body").on("click", ".fa-backward", function () {
     let id = $(this).closest(".podcast").find(".episodeName").attr("id")
     let pod = podManager.getCorrectPod(id)
-    pod.audioManager.audio.currentTime -=30
+    pod.audioManager.audio.currentTime -= 30
 })
 
 //same function - savedPodcasts
-$("body").on("click", ".save-play",  function () {
+$("body").on("click", ".save-play", function () {
     let id = $(this).closest(".row").find(".podcast").find(".episodeName").attr("id")
-    let pod = podManager.getCorrectSavedPod(id) 
+    let pod = podManager.getCorrectSavedPod(id)
     pod.audioManager.audio.play()
 })
 
 $("body").on("click", ".save-pause", async function () {
     let id = $(this).closest(".row").find(".podcast").find(".episodeName").attr("id")
-    let pod = podManager.getCorrectSavedPod(id)    
+    let pod = podManager.getCorrectSavedPod(id)
     pod.audioManager.audio.pause()
 })
 
 $("body").on("click", ".save-stop", async function () {
     let id = $(this).closest(".row").find(".podcast").find(".episodeName").attr("id")
-    let pod = podManager.getCorrectSavedPod(id)    
+    let pod = podManager.getCorrectSavedPod(id)
     pod.audioManager.audio.pause()
     pod.audioManager.audio.currentTime = 0;
 
@@ -96,7 +88,7 @@ $("body").on("click", ".carusela-play ", async function () {
     console.log("play?")
     let id = $(this).closest(".podcast").find(".episodeName").attr("id")
     console.log(id)
-    let pod = discoveryManager.getCorrectDiscovery(id)    
+    let pod = discoveryManager.getCorrectDiscovery(id)
     pod.audioManager.audio.play()
 })
 
@@ -116,89 +108,91 @@ $("body").on("click", ".carusela-stop", function () {
 ////same functions -- played
 $("body").on("click", ".played-play ", async function () {
     console.log("play?")
-    let id = $(this).closest(".podcast").find(".episodeName").attr("id")
+    let id = $(this).closest(".row").find(".podcast").find(".episodeName").attr("id")
     console.log(id)
-    let pod = podManager.getCorrectListendPod(id)    
+    let pod = podManager.getCorrectListendPod(id)
     pod.audioManager.audio.play()
 })
 
 $("body").on("click", ".played-pause", async function () {
-    let id = $(this).closest(".podcast").find(".episodeName").attr("id")
-    let pod = podManager.getCorrectListendPod(id)    
+    let id = $(this).closest(".row").find(".podcast").find(".episodeName").attr("id")
+    let pod = podManager.getCorrectListendPod(id)
     pod.audioManager.audio.pause()
 })
 
 $("body").on("click", ".played-stop", function () {
     console.log("stop?")
 
-    let id = $(this).closest(".podcast").find(".episodeName").attr("id")
-    let pod = podManager.getCorrectListendPod(id)    
+    let id = $(this).closest(".row").find(".podcast").find(".episodeName").attr("id")
+    let pod = podManager.getCorrectListendPod(id)
     pod.audioManager.audio.pause()
     pod.audioManager.audio.currentTime = 0;
 
+})
+
     //==============================
 
-$("body").on("click", ".save", function () {
-    let podId = $(this).closest(".podcast").find(".episodeName").attr("id")
-    podManager.savePod(podId)
-    renderer.renderSaved(podManager.savedPodcast)
-    M.toast({html: 'Added to favorites!'})
-})
+    $("body").on("click", ".save", function () {
+        let podId = $(this).closest(".podcast").find(".episodeName").attr("id")
+        podManager.savePod(podId)
+        renderer.renderSaved(podManager.savedPodcast)
+        M.toast({ html: 'Added to favorites!' })
+    })
 
-$("body").on("click", ".remove", function () {
-    let podId = $(this).closest(".podcast").find(".episodeName").attr("id")
-    podManager.deletePod(podId)
-})
-
-
+    $("body").on("click", ".remove", function () {
+        let podId = $(this).closest(".podcast").find(".episodeName").attr("id")
+        podManager.deletePod(podId)
+    })
 
 
-$("body").on("click", ".time", function () {
-    let time = $(this).data().id
-    console.log(time)
-    discoveryManager.time = time
-    console.log(discoveryManager._time)
-    renderer.renderLang(discoveryManager._langArray)
-    $(".time").closest("#discovery-time-container").fadeOut()
-
-})
-
-$("body").on("click", ".language", function () {
-    let lang = $(this).data().id
-    discoveryManager.lang = lang
-    renderer.renderGenres(discoveryManager._genreIdAndName)
-    $(this).closest(".flags-container").fadeOut()
-})
-
-$("body").on("click", ".genres", async function () {
-    let genre = $(this).attr("data-name")
-    let genreID = $(this).attr("data-id")
-    discoveryManager.genre = genre
-    discoveryManager.genreId = genreID
-
-    $(".loader").css("display", "block")
-    await discoveryManager.discoverPodcasts()
-    $(".loader").css("display", "none")
-    $(this).closest(".genres-container").fadeOut()
-    await discoveryManager.discoverPodcasts()
-    renderer.renderDiscovered(discoveryManager.discoveredPodcasts)
-    $('.carousel').carousel()
 
 
-})
+    $("body").on("click", ".time", function () {
+        let time = $(this).data().id
+        console.log(time)
+        discoveryManager.time = time
+        console.log(discoveryManager._time)
+        renderer.renderLang(discoveryManager._langArray)
+        $(".time").closest("#discovery-time-container").fadeOut()
 
-$(document).keypress(function (e) {
-    var key = e.which
-    if (key === 13) {
-        handleSearch($(".userInput").val())
-        $(".userInput").val("")
+    })
+
+    $("body").on("click", ".language", function () {
+        let lang = $(this).data().id
+        discoveryManager.lang = lang
+        renderer.renderGenres(discoveryManager._genreIdAndName)
+        $(this).closest(".flags-container").fadeOut()
+    })
+
+    $("body").on("click", ".genres", async function () {
+        let genre = $(this).attr("data-name")
+        let genreID = $(this).attr("data-id")
+        discoveryManager.genre = genre
+        discoveryManager.genreId = genreID
+
+        $(".loader").css("display", "block")
+        await discoveryManager.discoverPodcasts()
+        $(".loader").css("display", "none")
+        $(this).closest(".genres-container").fadeOut()
+        await discoveryManager.discoverPodcasts()
+        renderer.renderDiscovered(discoveryManager.discoveredPodcasts)
+        $('.carousel').carousel()
+
+
+    })
+
+    $(document).keypress(function (e) {
+        var key = e.which
+        if (key === 13) {
+            handleSearch($(".userInput").val())
+            $(".userInput").val("")
+        }
+    })
+
+    const loadPage = async function () {
+        await podManager.getDataFromDB()
+        renderer.renderSaved(podManager.savedPodcast)
+        renderer.renderListened(podManager.listenedPodcast)
     }
-})
 
-const loadPage = async function () {
-    await podManager.getDataFromDB()
-    renderer.renderSaved(podManager.savedPodcast)
-    renderer.renderListened(podManager.listenedPodcast)
-}
-
-loadPage()
+    loadPage()

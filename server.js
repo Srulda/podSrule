2a2ba208d335985bd60ae6471ed04736351211f5
Danny <dannybrudner@gmail.com>
2019-04-18 02:56:35 -0400

    Added heroku variables.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const api = require('./server/routes/api')
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/podcastDB', { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/podcastDB', { useNewUrlParser: true })
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -14,6 +14,6 @@ app.use(express.static(path.join(__dirname, 'node_modules')))
 app.use('/', api)
 
 const port = 7070
-app.listen(port, function () {
+app.listen(process.env.PORT || port, function () {
     console.log(`Server running on ${port}`)
 })
